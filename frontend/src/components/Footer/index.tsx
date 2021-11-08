23d89d3c7bcceb6541275932cc149c0e73aa63a7
José Alarcón <dev.josealarcon@gmail.com>
2021-11-08 17:54:14 -0300

    Footer

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
   <div className="container">
-    <p className="text-light">App desenvolvido por <a href="https://github.com/dev-josealarcon" target="_blank" rel="noreferrer">GitHub José Alarcón</a></p>
+    <p className="text-light">App desenvolvido por <a href="https://github.com/dev-josealarcon" target="_blank" rel="noreferrer">GitHub</a></p>
     <p className="text-light"><small><strong>Spring React</strong><br/>
        <a href="https://jose-alarcon.com" target="_blank" rel="noreferrer">José Alarcón</a></small></p>
   </div>
