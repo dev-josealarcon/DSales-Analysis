e654b860991b38e3d8a76a6ef9ec8523cb2c5e13
José Alarcón <dev.josealarcon@gmail.com>
2021-11-12 09:44:06 -0300

    Home,Navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,16 +1,16 @@
 const Footer = () => {
-    return (
-        <footer className="footer mt-auto py-3 bg-dark">
-  <div className="container">
-    <p className="text-light">App desenvolvido por <a href="https://github.com/dev-josealarcon" target="_blank" rel="noreferrer">GitHub</a></p>
-    <p className="text-light"><small><strong>Spring React</strong><br/>
-       <a href="https://jose-alarcon.com" target="_blank" rel="noreferrer">José Alarcón</a></small></p>
-  </div>
-</footer>
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App developed by <a href="https://jose-alarcon.com" target="_blank" rel="noreferrer"> José Alarcón</a></p>
+        <p className="text-light"><small><strong>Spring React</strong><br />
+          <a href="https://jose-alarcon.com" target="_blank" rel="noreferrer">GitHub</a></small></p>
+      </div>
+    </footer>
 
 
-      
-    );
-  }
-  
+
+  );
+}
+
 export default Footer;
