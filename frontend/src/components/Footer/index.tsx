b87c0ccc89ca4ab6f4c7e91622f04c3e36d52276
José Alarcón <dev.josealarcon@gmail.com>
2021-11-13 07:18:45 -0300

    v.1.1

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,8 +3,8 @@ const Footer = () => {
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
         <p className="text-light">App developed by <a href="https://jose-alarcon.com" target="_blank" rel="noreferrer"> José Alarcón</a></p>
-        <p className="text-light"><small><strong>Spring React 2021</strong><br />
-          <a href="https://github.com/dev-josealarcon/project-sds3" target="_blank" rel="noreferrer">GitHub <i className="fab fa-github"></i></a></small></p>
+        <p className="text-light"><small>
+          <a href="https://github.com/dev-josealarcon/project-sds3" target="_blank" rel="noreferrer">GitHub <i className="fab fa-github"></i></a> <strong> Spring React 2021</strong></small></p>
       </div>
     </footer>
 
