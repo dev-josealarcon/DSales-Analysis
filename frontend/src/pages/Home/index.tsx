d5e80ce178a179c12d36dbb67e5e4299f79955ed
José Alarcón <dev.josealarcon@gmail.com>
2021-11-16 16:12:20 -0300

    v1.2

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -8,11 +8,11 @@ const Home = () => {
         <NavBar />
         <div className="container">
             <div className="jumbotron">
-                <h1 className="display-4">DSale</h1>
+                <h1 className="display-4">DSale Analysis</h1>
                 <p className="lead">Analyze your sales performance from different perspectives.</p>
                 <hr />
                 <p>This application consists of displaying a dashboard from data provided by a back end built with Spring Boot.<br /><br />
-                Technologies: <br /><br />
+                Used technology: <br /><br />
                 <span>FRONT-END:</span> HTML5 <i className="fab fa-html5"></i> - CSS3 <i className="fab fa-css3-alt"></i>- JAVASCRIPT (React JS) <i className="fab fa-react"></i><br/>
                 <span>BACK-END: </span> JAVA(Spring tool Suite) <i className="fab fa-java"></i><br />
                 <span>DATABASE:</span> PostgreSQL <i className="fas fa-database"></i></p>
