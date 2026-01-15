c44bacb15be8ad224ddc6af0e3655a76dc20f829
José Alarcón <88212341+dev-josealarcon@users.noreply.github.com>
2026-01-14 22:00:58 -0500

    Update index.tsx

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container">
-        <p className="text-light">2021 App developed by <a href="https://jose-alarcon.com" target="_blank" rel="noreferrer"> José Alarcón</a></p>
+        <p className="text-light">2021 App developed by <a href="https://dev-josealarcon.github.io/site/" target="_blank" rel="noreferrer"> José Alarcón</a></p>
         <p className="text-light"><small> Repository  
           <a href="https://github.com/dev-josealarcon/project-sds3" target="_blank" rel="noreferrer"> GitHub <i className="fab fa-github"></i></a> <strong> </strong></small></p>
       </div>
