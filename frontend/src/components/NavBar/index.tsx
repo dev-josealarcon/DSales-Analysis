3b7ea77e1d6b8fc1f30b931eecb3a36cc58b52fe
José Alarcón <88212341+dev-josealarcon@users.noreply.github.com>
2026-01-14 22:07:05 -0500

    Update index.tsx

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div className="d-flex flex-column flex-md-row align-items-center px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
         <nav className="my-2 my-md-0 mr-md-3">
-          <Link to="/" className="mark1">
+          <Link to="https://dev-josealarcon.github.io/site/#projects/4" className="mark1">
             <div className="mark" >
               <div className="logo">
                 <div></div>
